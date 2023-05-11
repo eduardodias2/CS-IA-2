2ed65976a6a5d2fc0b53949261a80d37162f66dd
eduardodias2 <eduuu.d.06@gmail.com>
2023-05-10 23:56:03 -0300

    register page commit

---
+++ src/Pages/Register/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    padding-top: 9.5%;
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border: 2px solid ${({theme}) => theme.COLORS.BACKGROUND_500};
+        border-radius: 1rem;
+        padding: 1.5rem;
+        width: 50rem;
+        margin-top: 3rem;
+        text-align: center;
+        > button {
+          width: 13.5rem;
+          margin: auto;
+        }
+    }
+    p {
+      font-size: 1.35rem;
+      margin-top: 1.5rem;
+    }
+    a {
+      text-decoration: none;
+      color: ${({theme}) => theme.COLORS.BACKGROUND_E_500};
+    }
+    a:hover {
+      font-weight: bold;
+    }
+`
+
--- src/routers/auth.routes.jsx
+++ src/routers/auth.routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login } from '../Pages/Login';
+import { Register } from '../Pages/Register';
 
 
 export function AuthRoutes () {
   return (
     <Routes>
       <Route path="/" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
     </Routes>
   )
 }
